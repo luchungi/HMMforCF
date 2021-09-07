704e9dd2556fb6364531d0905b391d044fb9a520
Lu Chung I <lu.chung.i@gmail.com>
2021-09-07 14:59:27 +0800

    process data

--- HMMforCF.py
+++ HMMforCF.py
@@ -1,3 +1,24 @@
-#%%
+# %%
 import numpy as np
 import pandas as pd
+from datetime import datetime
+# %%
+path = 'D:\\Netflix Dataset\\'
+file = 'combined_data_1.txt'
+nrows = 1000000
+df = pd.read_csv(path + file, sep=',', header=None, names=['user', 'rating'], nrows=nrows)
+# %%
+movie_id_index = pd.DataFrame(pd.isnull(df['rating']))
+df_movies = df[df['rating'].isna()]
+df_movies = df_movies['user']
+df_movies = df_movies.rename('movie_id')
+df_movies = df_movies.map(lambda x: x.rstrip(':'))
+np_movies = np.stack([df_movies.index.to_numpy(), df_movies.to_numpy()], axis=-1)
+
+# %%
+df['movie_id'] = None
+
+for i in range(np_movies.shape[0] - 1):
+    # df.iloc[np_movies[i, 0]+1: np_movies[i+1, 0]]['movie_id'] = np_movies[i, 1]
+    df.loc[(np_movies[i,0]+1):(np_movies[i+1,0]-1), 3] = np_movies[i, 1]  
+# %%
